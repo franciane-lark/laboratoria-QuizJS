c6d8a6bee969b88eecfeb677949a3063b9dad230
Franciane Lark <franciane.lark019@gmail.com>
2023-02-13 13:46:23 -0400

    update desafio

---
+++ script.js
@@ -0,0 +1,16 @@
+function myfunction1() {
+  alert("A resposta é Verde e Amarelo");
+};
+
+function myfunction2() {
+  alert("O novo presidente do Brasil é Lulaa!");
+};
+
+function myfunction3() {
+  alert("Touro!");
+};
+
+function enviar(){
+  var valor = document.getElementById("Nome").value;
+  alert ("Olá, " + valor);
+}
